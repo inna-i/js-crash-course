ff9010fcb436c808f58255dae0a1605971211b82
inna-i <iivashchuk@talend.com>
2021-11-17 09:46:45 +0200

    fix: make cakes fn faster

--- arrays/cakes.js
+++ arrays/cakes.js
@@ -1,29 +1,33 @@
-function canIMakeCake(obj1, obj2, n) {
-    console.log(arguments);
+function canIMakeCake(obj1, obj2) {
     const result = {};
     let canIMake = true;
-    let max = 0;
+    let max = Infinity;
     let missedIngredient = 'All good';
 
-    Object.keys(obj1).forEach(ingredient => {
+    for (let ingredient in obj1) {    
         if (obj2[ingredient] === 0) {
             result[ingredient] = 0;
             canIMake = false;
             missedIngredient = ingredient;
-        } else if(obj2[ingredient]) {
-            result[ingredient] = parseInt(obj2[ingredient]/obj1[ingredient]);
-        }
-    });
 
-    if (canIMake) {
-        max = Math.min(...Object.values(result));
+        } else if(obj2[ingredient]) {
+            const amount = parseInt(obj2[ingredient]/obj1[ingredient]);
+            result[ingredient] = amount;
+
+            if (max > amount) {
+                max = amount;
+            }
+        }
+    }
+
+    if (!canIMake) {
+        max = 0;
     }
 
     return { canIMake, max, missedIngredient, result};
 }
 
 console.log(canIMakeCake(
-    {flour: 500, sugar: 200, eggs: 1}, 
+    {flour: 500, sugar: 200, eggs: 2}, 
     {flour: 1200, sugar: 1200, eggs: 2, milk: 200},
-    10
 ));
