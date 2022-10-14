5676dc5eadec0451ec0879a2a7ba90a6d143e1ae
inna-i <iivashchuk@talend.com>
2022-10-14 09:57:41 +0300

    feat: functions examples

--- funcs/Animal.js
+++ funcs/Animal.js
@@ -8,14 +8,20 @@ class Animal {
     sayHello() {
       return "Hello! My name is " + this.name + " and I am a " + this.type;
     }
+
+    makeSound(sound) {
+      return `${sound}! ${sound}!`;
+    }
 }
   
 // cat 
 const Cat = new Animal("Felix", "cat");
 // call a method
 console.log(Cat.sayHello());
+console.log(Cat.makeSound("Meow"));
 
 // dog 
 const Dog = new Animal("Beethoven", "dog");
 // call a method
 console.log(Dog.sayHello());
+console.log(Cat.makeSound("Woof"));
--- funcs/diffrentFunctions.js
+++ funcs/diffrentFunctions.js
@@ -2,7 +2,7 @@ const square = (a) => {
     return a * a
 };
 
-const square1 = (a) => a * a;
+const square1 = a => a * a;
 
 function square2(a) {
     return a * a;
@@ -16,5 +16,69 @@ const square4 = function(a, cb) {
     return cb(a);
 }
 
-console.log('Fn expression ', square3(10));
-console.log('Fn with anonymous fn', square4(10, square1));
+// console.log('Arrow fn ', square(10));
+// console.log('Fn expression ', square3(10));
+// console.log('result 1 ', calc(10, 5));
+
+// function declaration
+function calc(a, b) {
+    return a + b;
+}
+
+// function expression
+const calc2 = function(a, b) {
+    return a + b;
+}
+// console.log('result 2 ', calc2(10, 5));
+
+const calc3 = function(a, b, c, d) {
+    let result = a + b;
+
+    // if (Number.isInteger(c) && result > c) {
+    if (typeof c === 'number' && result > c) {
+        result -= c;
+    }
+
+    if (d && d > 0) {
+        result = result / d;
+    }
+
+    return result;
+}
+
+const resultCalc3 = calc3(10, 10, 4);
+
+// console.log('result ', resultCalc3);
+
+
+const calculator = function(num1, num2, type) {
+    const sum = (a,b) => a + b;
+
+    const division = (a, b) => a / b;
+    
+    const sumDivision = (a, b) => (a + b) / b;
+
+    const square = function(a) {
+        return a * a;
+    }
+
+    switch(type) {
+        case 'sum':
+            return sum(num1, num2);       
+        case 'division':
+            return division(num1, num2);
+        case 'sumDivision':
+            return sumDivision(num1, num2);        
+        case 'square':
+            return square(num1, num2);
+
+        default:
+            console.error('Please provide numbers')
+    }
+}
+
+// const result = calculator();
+
+console.log(result)
+
+export function myFunc (a, b) {}
